3433edb404f453b00b3166fb7a885d15977c4c6c
Gökberk Karaca <gskaraca1905@gmail.com>
2018-05-10 19:26:38 +0300

    Add config.php for database connection

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	define('db_host_address', 'dijkstra.ug.bcc.bilkent.edu.tr');
+	define('db_username', 'gokberk.karaca');
+	define('db_password', 'pnsank5');
+	define('db_name', 'gokberk_karaca');
+	$db_connection = mysqli_connect(db_host_address, db_username, db_password, db_name);
+?>
