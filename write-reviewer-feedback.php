f17553a6c5719efa57e87a5a3b760da2dc3159ff
Gokberk Karaca <gskaraca1905@gmail.com>
2018-05-12 13:36:15 +0300

    Edit index page

---
+++ write-reviewer-feedback.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Write Feedback</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="form-group" align="center">
+    <label for="comment">Feedback:</label>
+    <textarea class="form-control" rows="5" id="comment"></textarea>
+    <button type="submit" class="btn btn-success">Save</button>
+  </div>
+  </body>
+</html>
