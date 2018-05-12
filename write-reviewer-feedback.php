2094d08ff2284e32b92ccba7e0c67be317a5406c
Gokberk Karaca <gskaraca1905@gmail.com>
2018-05-12 21:41:24 +0300

    Add functionality to write-reviewer-feedback.php

--- write-reviewer-feedback.php
+++ write-reviewer-feedback.php
@@ -1,4 +1,17 @@
 <!DOCTYPE html>
+<?php
+  require('config.php');
+  session_start();
+  if(isset($_GET['s_id']) && isset($_GET['editor_email'])) {
+    $s_id = $_GET['s_id'];
+    $editor_email = $_GET['editor_email'];
+    $reviewer_email = $_SESSION['email'];
+  }
+  else {
+    // TODO Close window
+  }
+ ?>
+
 <html>
   <head>
     <meta charset="utf-8">
@@ -6,10 +19,22 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
   </head>
   <body>
-  <div class="form-group" align="center">
-    <label for="comment">Feedback:</label>
-    <textarea class="form-control" rows="5" id="comment"></textarea>
-    <button type="submit" class="btn btn-success">Save</button>
-  </div>
+  <form method="POST" action="">
+    <div class="form-group" align="center">
+      <label for="comment">Feedback:</label>
+      <textarea class="form-control" rows="5" id="comment" name="feedback"></textarea>
+      <br />
+      <button type="submit" class="btn btn-success" name="save">Save</button>
+      <?php
+        if (isset($_POST['save'])) {
+          if (isset($_POST['feedback'])) {
+            $feedback = $_POST['feedback'];
+            $sql = "INSERT INTO reviews VALUES(\"$reviewer_email\", \"$editor_email\", \"$s_id\",  \"$feedback\")";
+            echo "<script>window.close();</script>";
+          }
+        }
+      ?>
+    </div>
+  </form>
   </body>
 </html>
