cb5533a923ce04de10980d134226a74e7c842763
Ali Sabbagh <ali96sab@gmail.com>
2018-05-13 03:27:37 +0300

    signup.css fix link

--- signup.php
+++ signup.php
@@ -2,7 +2,7 @@
 <html>
     <head>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous" />
-            <link rel="stylesheet" href="signup.css" />
+            <link rel="stylesheet" href="css/signup.css" />
             <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
             <script src="js/signup.js"></script>
     </head>
